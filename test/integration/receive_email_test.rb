871ac5d0b4bb35b801a2a803f7b15b5484400c84
Eric Davis <edavis@littlestreamsoftware.com>
2011-02-11 09:46:04 -0800

    [#3766] Add a context around replying to issues

--- test/integration/receive_email_test.rb
+++ test/integration/receive_email_test.rb
@@ -1,60 +1,62 @@
 require 'test_helper'
 
 class ReceiveEmailTest < ActionController::IntegrationTest
-  should "allow receiving email from email watchers to edit an issue" do
-    generate_user_as_project_manager
+  context "from a issue email" do
+    should "allow receiving email from email watchers to edit an issue" do
+      generate_user_as_project_manager
 
-    login_as
-    visit_issue_page(@issue)
+      login_as
+      visit_issue_page(@issue)
 
-    add_email_watcher_through_form 'jsmith@somenet.foo'
+      add_email_watcher_through_form 'jsmith@somenet.foo'
 
-    mail = IO.read(File.dirname(__FILE__) + '/../fixtures/incoming_mail/reply.eml')
-    mail.gsub!('{{issue-id}}', @issue.id.to_s)
+      mail = IO.read(File.dirname(__FILE__) + '/../fixtures/incoming_mail/reply.eml')
+      mail.gsub!('{{issue-id}}', @issue.id.to_s)
 
-    assert_difference('Journal.count') do
-      MailHandler.receive(mail)
+      assert_difference('Journal.count') do
+        MailHandler.receive(mail)
+      end
+
+    end
+
+    should "allow receiving email from email watchers to edit an issue on private projects" do
+      generate_user_as_project_manager
+      @project.update_attribute(:is_public, false)
+      @project.reload
+
+      login_as
+      visit_issue_page(@issue)
+
+      add_email_watcher_through_form 'jsmith@somenet.foo'
+
+      mail = IO.read(File.dirname(__FILE__) + '/../fixtures/incoming_mail/reply.eml')
+      mail.gsub!('{{issue-id}}', @issue.id.to_s)
+
+      assert_difference('Journal.count') do
+        MailHandler.receive(mail)
+      end
+
+    end
+
+    should "allow not all receiving email from email watchers to a different issue" do
+      generate_user_as_project_manager
+
+      login_as
+      visit_issue_page(@issue)
+
+      add_email_watcher_through_form 'jsmith@somenet.foo'
+
+      # Second issue they aren't watching
+      @issue2 = Issue.generate_for_project!(@project)
+      
+      mail = IO.read(File.dirname(__FILE__) + '/../fixtures/incoming_mail/reply.eml')
+      mail.gsub!('{{issue-id}}', @issue2.id.to_s)
+
+      assert_difference('Journal.count', 0) do
+        MailHandler.receive(mail)
+      end
+
     end
 
   end
-
-  should "allow receiving email from email watchers to edit an issue on private projects" do
-    generate_user_as_project_manager
-    @project.update_attribute(:is_public, false)
-    @project.reload
-
-    login_as
-    visit_issue_page(@issue)
-
-    add_email_watcher_through_form 'jsmith@somenet.foo'
-
-    mail = IO.read(File.dirname(__FILE__) + '/../fixtures/incoming_mail/reply.eml')
-    mail.gsub!('{{issue-id}}', @issue.id.to_s)
-
-    assert_difference('Journal.count') do
-      MailHandler.receive(mail)
-    end
-
-  end
-
-  should "allow not all receiving email from email watchers to a different issue" do
-    generate_user_as_project_manager
-
-    login_as
-    visit_issue_page(@issue)
-
-    add_email_watcher_through_form 'jsmith@somenet.foo'
-
-    # Second issue they aren't watching
-    @issue2 = Issue.generate_for_project!(@project)
-    
-    mail = IO.read(File.dirname(__FILE__) + '/../fixtures/incoming_mail/reply.eml')
-    mail.gsub!('{{issue-id}}', @issue2.id.to_s)
-
-    assert_difference('Journal.count', 0) do
-      MailHandler.receive(mail)
-    end
-
-  end
-
 end
